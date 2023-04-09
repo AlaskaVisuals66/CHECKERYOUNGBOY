3466eff97edcceb17d5e5e45664ae5a65406f34a
AlaskaVisuals66 <78333678+AlaskaVisuals66@users.noreply.github.com>
2023-04-09 06:06:40 -0300

    Update chatgpt_bypass.sh

--- chatgpt_bypass.sh
+++ chatgpt_bypass.sh
@@ -6,13 +6,13 @@ echo "\n[+] Output:"
 curl=`cat <<EOS
 curl -s https://api.openai.com/v1/completions \
   -H 'Content-Type: application/json' \
-  -H "Authorization: Bearer $CHATGPT_TOKEN" \
+  -H "Authorization: Bearer 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" \
   -d '{
   "model": "text-davinci-003",
   "prompt": "$1",
   "max_tokens": 4000,
   "temperature": 1.0
 }' \
---insecure | jq -r '.choices[]'.text
+--insecure | jq -r '.choices[].text'
 EOS`
 eval ${curl}
